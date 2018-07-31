e7de2fe2b57c1673a2eed68fb2fdfb1cb564595c
Dominggoes Isakh <drjisakh@gmail.com>
2018-07-31 15:12:39 +0200

    libsensors: Implement flush-method for AccelSensor
    This fixes the accelerometer in Oreo for
    SENSORS_DEVICE_API_VERSION_1_3.
    
    Change-Id: I219872ed24374bff1124d3bdf6c112b49311a0df

--- libsensors/AccelSensor.h
+++ libsensors/AccelSensor.h
@@ -48,6 +48,7 @@ struct input_event;
 
 class AccelSensor : public SensorBase {
     int mEnabled;
+    uint32_t mFlushed;
     InputEventCircularReader mInputReader;
     sensors_event_t mPendingEvent;
     bool mHasPendingEvent;
@@ -63,6 +64,8 @@ public:
     virtual bool hasPendingEvents() const;
     virtual int setDelay(int32_t handle, int64_t ns);
     virtual int enable(int32_t handle, int enabled);
+    virtual int batch(int handle, int flags, int64_t period_ns, int64_t timeout);
+    virtual int flush(int handle);
 };
 
 /*****************************************************************************/
