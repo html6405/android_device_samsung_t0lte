4fbdff00df937e3eacbaa5bc224d90571ee11636
Danilo Santos <dansantosbe@gmail.com>
2018-10-06 21:08:57 -0300

    t0lte: tree reworked
    Change-Id: I7e5170fee232b755d3eab3e0bad8f1ae1db0668e

--- lineage_t0lte.mk
+++ lineage_t0lte.mk
@@ -5,11 +5,12 @@ PRODUCT_RELEASE_NAME := t0lte
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/samsung/t0lte/t0lte.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0lte
