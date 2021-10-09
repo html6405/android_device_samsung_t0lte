084bfcc4f193136e70a7d03d6a7b2792a611b223
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-10-09 12:10:38 +0200

    t0lte.mk move android.hardware.telephony.gsm.xml permissions to vendor.

--- t0lte.mk
+++ t0lte.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := device/samsung/t0lte
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:vendor/etc/permissions/android.hardware.telephony.gsm.xml
 
 # Include common makefile
 $(call inherit-product, device/samsung/t0lte/common.mk)
