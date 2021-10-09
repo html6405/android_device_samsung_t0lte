ec7ce1bf641b965ed87e4128071331c491391387
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-10-09 12:16:24 +0200

    Convert system.prop to vendor_prop.mk.

--- common.mk
+++ common.mk
@@ -111,5 +111,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # Include common makefile
 $(call inherit-product, vendor/samsung/t0lte/t0lte-vendor.mk)
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/smdk4412-qcom-common/common.mk)
