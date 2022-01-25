42fe1bdaf2d7d272d5ff11a1385bedd890c8f8aa
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-01-25 18:51:59 +0100

    libsensors: Android.mk: fix build and move to vendor/hw.

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -22,9 +22,8 @@ ifneq ($(TARGET_SIMULATOR),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := sensors.$(TARGET_BOOTLOADER_BOARD_NAME)
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS := -DALOG_TAG=\"Sensors\"
@@ -39,7 +38,7 @@ LOCAL_SRC_FILES := 						\
                                 AccelSensor.cpp                 \
                                 PressureSensor.cpp
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libhardware
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
